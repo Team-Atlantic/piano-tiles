68429c70329ef2d85303f366c2cf22a7501c412b
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-10-01 20:50:21 +0530

    game board now moving

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 `use scrict`;
 document.addEventListener("DOMContentLoaded", function () {
+  let score=0;
   let activeTile = generateRandomNum();
 
-  const gameTime = 1000;
+  const gameTime = 2000;
  
   const keyMap = { 0: "A", 1: "S", 2: "D", 3: "F" };
+  
+  const gameBoard = document.querySelector(".game-board");
+  setInterval(function(){
+    let sec=document.createElement("section");
+    let ranNo=generateRandomNum();
+    sec.className="board-row";
+    for(let i=0;i<=3;i++){
+      let para=document.createElement("p");
+      para.className="row";
+      para.addEventListener('click',CheckBgcolor);
+      if(i===ranNo){
+        para.style.background="black"
+      }else{
+        para.style.background="white"
+      }
+      sec.appendChild(para);
+    }
+    gameBoard.appendChild(sec);
+    setInterval(removeChild,gameTime*4,sec);
+  },gameTime)
 
 
+ 
+function removeChild(sect){
+  sect.remove();
+}
+
+
+function CheckBgcolor(event){
+  // console.log('event',event)
+  console.log(getComputedStyle(event.target).backgroundColor)
+  if(getComputedStyle(event.target).backgroundColor==="rgb(0, 0, 0)"){
+    score+=1
+    console.log('score',score)
+  }else{
+    alert("game over!")
+  }
+
+}
 
   function setTile(prev, cur) {
     let row = document.querySelectorAll(".row");
