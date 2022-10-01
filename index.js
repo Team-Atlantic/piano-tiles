a472b9e4db8ceba1ed06ed2e3a29573badd017b6
Nitish <rdpara1214@gmail.com>
2022-10-01 18:42:59 +0530

    changes for piano

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
   let activeTile = generateRandomNum();
 
   const gameTime = 1000;
-  let tiles = Array(4).fill(0);
+ 
   const keyMap = { 0: "A", 1: "S", 2: "D", 3: "F" };
 
-  // tiles[activeTile] = 1;
-  console.log(tiles);
+
 
   function setTile(prev, cur) {
     let row = document.querySelectorAll(".row");
     row[prev].style.backgroundColor = "";
-    //   tiles[prev] = 0;
-    //   tiles[cur] = 1;
+  
     row[cur].innerText = keyMap[cur];
     row[cur].style.backgroundColor = "black";
   }
@@ -28,5 +26,5 @@ document.addEventListener("DOMContentLoaded", function () {
     return ((Math.random() * 10) | 0) % 4;
   }
 
-  setInterval(generateBoard, gameTime);
+  // setInterval(generateBoard, gameTime);
 });
