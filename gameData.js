7db1681cc0029903e3315ecafb33518959a0ca7c
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-10-03 17:51:29 +0530

    flash

--- gameData.js
+++ gameData.js
@@ -1,11 +1,11 @@
-import {mygame,startTheGame} from './index.js';
-export let score=0;
+import { mygame, startTheGame } from "./index.js";
+export let score = 0;
 export let prevScore = 0;
 export let gameTime = 1000;
 
 export const keyMap = { 0: "A", 1: "S", 2: "D", 3: "F" };
 
-export const keyCodeMap ={65:0,83:1,68:2,70:3};
+export const keyCodeMap = { 65: 0, 83: 1, 68: 2, 70: 3 };
 
 const mediumLevel = 10;
 export const mediumgameTime = 500;
@@ -14,49 +14,51 @@ export const easygameTime = 1000;
 const hardLevel = 20;
 export const hardgameTime = 400;
 
-const levelTag = document.querySelector('.level');
+const levelTag = document.querySelector(".level");
 
-export function updateScore(){
-    score +=1;
+export function updateScore() {
+  score += 1;
 
-    if (score>mediumLevel && score<hardLevel){
-        increaseSpeed(mediumLevel);
-    }
-    else if(score >= hardLevel){
-        increaseSpeed(hardLevel);
-        
-    }
-   
+  if (score > mediumLevel && score < hardLevel) {
+    increaseSpeed(mediumLevel);
+  } else if (score >= hardLevel) {
+    increaseSpeed(hardLevel);
+  }
 }
 
-function increaseSpeed(level){
-    if (level === mediumLevel){
+function increaseSpeed(level) {
+  if (level === mediumLevel) {
+    setGameTime(mediumgameTime);
+    levelTag.classList.remove("easy");
+    levelTag.classList.add("medium");
+    document.querySelector(".showScore").classList.add("flash");
 
-        setGameTime(mediumgameTime);
-        levelTag.classList.remove('easy');
-        levelTag.classList.add('medium');
-        levelTag.textContent = "Medium";
-    }
-    else if(level == hardLevel){
-        setGameTime(hardgameTime);
-        levelTag.classList.remove('medium');
-        levelTag.classList.add('hard');
-        levelTag.textContent = "Hard";
-    }
-    clearTimeout(mygame);
-setTimeout(startTheGame,gameTime);
+    setTimeout(function () {
+      document.querySelector(".showScore").classList.remove("flash");
+    }, 1000);
+    levelTag.textContent = "Medium";
+  } else if (level == hardLevel) {
+    setGameTime(hardgameTime);
+    levelTag.classList.remove("medium");
+    levelTag.classList.add("hard");
+    levelTag.textContent = "Hard";
+
+    document.querySelector(".showScore").classList.add("flash");
+    setTimeout(function () {
+      document.querySelector(".showScore").classList.remove("flash");
+    }, 1000);
+  }
+  clearTimeout(mygame);
+  setTimeout(startTheGame, gameTime);
 }
 
-
-export function setGameTime(time){
-    gameTime = time
+export function setGameTime(time) {
+  gameTime = time;
 }
-export function updatePrevScore(prev){
-
-    prevScore = Math.max(prev,prevScore);
-
+export function updatePrevScore(prev) {
+  prevScore = Math.max(prev, prevScore);
 }
 
-export function resetScore(){
-    score = 0;
+export function resetScore() {
+  score = 0;
 }
--- score.js
+++ score.js
@@ -1,15 +1,10 @@
-import {
-  score,
-  prevScore,
-  gameTime,
-  updatePrevScore,
-  
-} from "./gameData.js";
+import { score, prevScore, gameTime, updatePrevScore } from "./gameData.js";
 import { setInitalState } from "./index.js";
 import { playGameOver } from "./audio.js";
 
 let scoreTimer = null;
 let countScore = document.querySelector(".currentScore");
+const level = document.querySelector(".level");
 // update the span currentScore with the score value.
 export function scoreCard() {
   countScore.innerText = score;
@@ -21,7 +16,7 @@ const scoreBoard = document.querySelector(".score-screen");
 const showScore = document.querySelector(".showScore");
 
 // It will be called when the game is over.
-export function gameOver(mygame,errorMsg) {
+export function gameOver(mygame, errorMsg) {
   playGameOver();
   // clearInterval(mygame);
   // stop adding or deleting rows in game board.
@@ -33,13 +28,12 @@ export function gameOver(mygame,errorMsg) {
   // gameBoard.innerHTML = showScoreHtml;
   gameBoard.innerHTML = "";
   scoreBoard.style.display = "revert";
-  showScore.style.display ="none";
+  showScore.style.display = "none";
   updatePrevScore(score);
   document.querySelector(".score span").innerText = score;
   document.querySelector(".best span").innerText = prevScore;
   document.querySelector(".error-msg").innerText = errorMsg;
-  document.querySelector("footer").style.display="revert";
-  
+  document.querySelector("footer").style.display = "revert";
 }
 
 // score-exit-btn
@@ -47,21 +41,27 @@ const exitBtn = document.querySelector(".score-exit-btn");
 exitBtn.addEventListener("click", function () {
   document.querySelector(".rule").style.display = "revert";
   document.querySelector(".score-screen").style.display = "none";
-  document.querySelector("footer").style.display="revert";
+  document.querySelector("footer").style.display = "revert";
 });
 
 const playAgainBtn = document.querySelector(".score-again-btn");
 
 // It will be called when we click play again.
 playAgainBtn.addEventListener("click", function () {
-  
   gameBoard.style.display = "grid";
   document.querySelector(".showScore").style.display = "revert";
-  document.querySelector("footer").style.display="none";
+  document.querySelector("footer").style.display = "none";
   // scoreCard();
   scoreBoard.style.display = "none";
   showScore.style.display = "revert";
   // startGame();
+  if (level.classList.contains("medium")) {
+    level.classList.remove("medium");
+  }
+  if (level.classList.contains("hard")) {
+    level.classList.remove("hard");
+  }
+  document.querySelector(".level").classList.add("easy");
 
   setInitalState();
 });
