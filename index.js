f0ec1e3beae85cc986bb0c2b62ace78be47b13f7
Nitish <rdpara1214@gmail.com>
2022-10-03 08:30:16 +0530

    removed confilt from index.js after merge with anjali design commit

--- index.js
+++ index.js
@@ -151,68 +151,6 @@ function checkPressedKey(event) {
   }
 }
 
-// // called when we click on start on rules div
-// export function startGame() {
-
-//   playGameStart();
-
-//   scoreCard();
-//   mygame = setInterval(function () {
-//     let sec = document.createElement("section");
-//     let ranNo = generateRandomNum();
-//     sec.className = "board-row";
-
-//     for (let i = 0; i <= 3; i++) {
-//       let para = document.createElement("p");
-//       para.className = "row";
-//       para.addEventListener("click", CheckBgcolor);
-
-//       if (i === ranNo) {
-//         para.style.background = "black";
-//         para.textContent = keyMap[i];
-//         para.classList.add("black-tile");
-//       } else {
-//         para.style.background = "white";
-//       }
-//       sec.appendChild(para);
-//     }
-
-//     gameBoard.appendChild(sec);
-//     sec.focus();
-//     // event for keyboard
-//     document.addEventListener("keydown", function (event) {
-//       let keyval = event.key;
-//       let blackTile = sec.querySelector(".black-tile");
-//       if (blackTile.classList.contains('counted')){
-//         return;
-//       }
-
-//       console.log(keyval, blackTile.textContent.toLowerCase());
-
-//       // && !blackTile.classList.contains("counted")
-//       if (keyval=== blackTile.textContent.toLowerCase()) {
-//         updateScore();
-//         // to mark that it should not be counted again.
-//         blackTile.classList.add("counted");
-//       } else {
-//         // .style.backgroundColor = "red";
-//         // .style.transition = "0.5s ease-in";
-//         setTimeout(gameOver, 500, mygame);
-//       }
-//     },false);
-//     setInterval(removeChild, gameTime * 4, sec);
-//   }, gameTime);
-// }
-
-// used for removing child from gameboard. If you miss the black tile --> game over.
-// function removeChild(sect) {
-//   let blackTile = sect.querySelector(".black-tile");
-//   let opacityofTile = getComputedStyle(blackTile).opacity;
-//   if (opacityofTile === "1") {
-//     gameOver(mygame);
-//   }
-//   sect.remove();
-// }
 
 // update the score if we click on black tile otherwise game over.
 function CheckBgcolor(event) {
